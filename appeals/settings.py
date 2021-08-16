72a9722dc65dc0795f3c388ba51aa11cf6629260
Randall.Gienko <rgienko@gmail.com>
2021-08-16 16:53:09 -0500

    Updated settings files

--- appeals/settings.py
+++ appeals/settings.py
@@ -31,12 +31,6 @@ ALLOWED_HOSTS = ['myappeals.herokuapp.com','localhost', 'appeals.azurewebsites.n
                  '65.119.155.98']
 
 
-proxy = 'https://8bgghzfatdmy2:2jhmc41c1dqes6n3ck043qyux3syia@us-east-shield-02.quotaguard.com:9294'
-
-os.environ['http_proxy'] = proxy
-os.environ['HTTP_PROXY'] = proxy
-os.environ['https_proxy'] = proxy
-os.environ['HTTPS_PROXY'] = proxy
 # Application definition
 
 INSTALLED_APPS = [
