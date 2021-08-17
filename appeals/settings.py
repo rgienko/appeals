adfa7c01b7373e499664c64f903d80ce9ba02f38
Randall.Gienko <rgienko@gmail.com>
2021-08-16 21:40:16 -0500

    Updated settings files

--- appeals/settings.py
+++ appeals/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['myappeals.herokuapp.com','localhost', 'appeals.azurewebsites.net', '127.0.0.1:8000', '127.0.0.1',
+ALLOWED_HOSTS = ['myappeals.herokuapp.com','localhost', 'myappeals.azurewebsites.net', '127.0.0.1:8000', '127.0.0.1',
                  '65.119.155.98']
 
 
